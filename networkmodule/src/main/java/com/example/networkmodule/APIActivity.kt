ab60b4e30d88a582b6368922fac2ea2262eda4fb
shivamdhammi-winzo <85114907+shivamdhammi-winzo@users.noreply.github.com>
2022-02-15 23:02:36 +0530

    creating network module

---
+++ networkmodule/src/main/java/com/example/networkmodule/APIActivity.kt
@@ -0,0 +1,11 @@
+package com.example.networkmodule
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class APIActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apiactivity)
+    }
+}
